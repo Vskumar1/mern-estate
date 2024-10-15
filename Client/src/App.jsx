e55d55220c37575cb546c1f5fd8e3aacfcf863ca
vsk <vskisback11@gmail.com>
2024-10-16 00:52:51 +0400

    route commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import{BrowserRouter,Route,Routes} from 'react-router-dom';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+
 
 export default function App() {
   return (
     <div>
       <h1 className='text-red-500'>Hello</h1>
+      <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/' element={<About/>}></Route>
+        <Route path='/' element={<Profile/>}></Route>
+      </Routes>
+      
+      
+      </BrowserRouter>
       
     </div>
   )
